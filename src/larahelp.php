bda4f225cd8cda17428d56f16d06ac6c9cc4e2d4
Aaron Adrian <aaroncadrian@gmail.com>
2018-01-18 12:09:01 -0600

    Added more helper functions

--- src/larahelp.php
+++ src/larahelp.php
@@ -12,3 +12,35 @@ function mix_asset_script($path, $secure = null)
         'secure' => $secure,
     ]);
 }
+
+function mix_asset_stylesheet($path, $secure = null)
+{
+    return view('larahelp::mix_asset_script', [
+        'path' => $path,
+        'secure' => $secure,
+    ]);
+}
+
+function stylesheet($path)
+{
+    return view('larahelp::stylesheet', [
+        'path' => $path,
+    ]);
+}
+
+function script($path)
+{
+    return view('larahelp::script', [
+        'path' => $path,
+    ]);
+}
+
+function load_js($path)
+{
+    return script($path);
+}
+
+function load_css($path)
+{
+    return stylesheet($path);
+}
