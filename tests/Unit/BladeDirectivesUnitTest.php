6b1ba60ea02a648d91f3f2c9de636f051f592d6d
Aaron Adrian <aaroncadrian@gmail.com>
2018-06-08 14:07:40 -0400

    Add test for js directive

--- tests/Unit/BladeDirectivesUnitTest.php
+++ tests/Unit/BladeDirectivesUnitTest.php
@@ -2,7 +2,17 @@
 
 namespace AaronAdrian\Larahelp\Tests\Unit;
 
-class BladeDirectivesUnitTest
-{
+use AaronAdrian\Larahelp\Tests\AbstractBladeTestCase;
 
+class BladeDirectivesUnitTest extends AbstractBladeTestCase
+{
+    /**
+     * @test
+     */
+    public function js_directive()
+    {
+        $input = "@js('http://examplecdn.com/something.js')";
+        $expected = '<?php js(\'http://examplecdn.com/something.js\'); ?>';
+        $this->assertEquals($expected, $this->compiler->compileString($input));
+    }
 }
