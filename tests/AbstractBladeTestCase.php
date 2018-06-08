77b97f35d302239935dc3dc1496efc18138b5f5d
Aaron Adrian <aaroncadrian@gmail.com>
2018-06-08 14:04:16 -0400

    Add abstract Blade test case

--- tests/AbstractBladeTestCase.php
+++ tests/AbstractBladeTestCase.php
@@ -2,7 +2,22 @@
 
 namespace AaronAdrian\Larahelp\Tests;
 
-class AbstractBladeTestCase
-{
+use Mockery as m;
+use Illuminate\View\Compilers\BladeCompiler;
 
+abstract class AbstractBladeTestCase extends TestCase
+{
+    protected $compiler;
+
+    public function setUp()
+    {
+        $this->compiler = new BladeCompiler(m::mock('Illuminate\Filesystem\Filesystem'), __DIR__);
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        m::close();
+        parent::tearDown();
+    }
 }
